c79a54d5e826a05c38e7eea8fcbfcb42cc320fe1
Elisabeth Henry <liz.henry@ouvaton.org>
2013-10-12 04:00:02 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject valisp "0.1.0-SNAPSHOT"
+  :description "A lispy flavour of Vala"
+  :url "https://github.com/lady-segfault/valisp"
+  :license {:name "GNU General Public License 2.0"
+            :url "http://www.gnu.org/licenses/gpl-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
