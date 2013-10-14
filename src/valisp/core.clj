2c0d1b14c88d500ffb7d88fa0eb09b731513b823
Elisabeth Henry <liz.henry@ouvaton.org>
2013-10-14 21:34:32 +0200

    Now compiles an entire file, and not only the first s-expr

--- src/valisp/core.clj
+++ src/valisp/core.clj
@@ -2,13 +2,25 @@
   (:require [valisp.parser :as parser])
   (:gen-class))
 
+
+(defn parse-stream [r]
+  (try 
+    (->
+     (read r)
+     parser/run-parse
+     println)
+    (parse-stream r)
+    (catch java.lang.RuntimeException e
+      nil)
+    (catch Exception e
+      (println (.getMessage e)))))
  
-(defn read-from-file [filename]
+(defn parse-from-file [filename]
   "Read an object from file"
   (with-open [r (java.io.PushbackReader.
                  (clojure.java.io/reader filename))]
     (binding [*read-eval* false]
-      (read r))))
+      (parse-stream r))))
 
 (defn -main
   "Main function"
@@ -18,7 +30,5 @@
     (do
       (-> args
           first
-          read-from-file
-          parser/run-parse
-          println))))
+          parse-from-file))))
   
